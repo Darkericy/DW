ac0bdb25e137c9a6d04c433d4891bb7a6c9f236e
Darkericy <447103942@qq.com>
2023-07-13 23:18:53 +0800

    推送

---
+++ CMakeFiles/DW.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DW.dir/DW/log.cc.o"
+  "CMakeFiles/DW.dir/DW/log.cc.o.d"
+  "lib/libDW.pdb"
+  "lib/libDW.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DW.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
