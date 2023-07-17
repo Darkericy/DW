37f8c7e2d77cba6f47f90d98b219dcfa9f873798
Darkericy <447103942@qq.com>
2023-07-17 14:32:12 +0800

    删除vscode配置文件

--- DW/thread.cc
+++ DW/thread.cc
@@ -8,6 +8,34 @@ namespace DW{
 
     static Logger::ptr g_logger = DW_LOG_NAME("system");
 
+    Semaphore::Semaphore(){
+        if(sem_init(&m_semaphore, 0, count)) {
+            throw std::logic_error("sem_init error");
+        }
+    }
+
+    Semaphore::Semaphore(uint32_t count) {
+        if(sem_init(&m_semaphore, 0, count)) {
+            throw std::logic_error("sem_init error");
+        }
+    }
+
+    Semaphore::~Semaphore() {
+        sem_destroy(&m_semaphore);
+    }
+
+    void Semaphore::wait() {
+        if(sem_wait(&m_semaphore)) {
+            throw std::logic_error("sem_wait error");
+        }
+    }
+
+    void Semaphore::notify() {
+        if(sem_post(&m_semaphore)) {
+            throw std::logic_error("sem_post error");
+        }
+    }
+
     Thread* Thread::GetThis() {
         return t_thread;
     }
--- DW/thread.h
+++ DW/thread.h
@@ -5,14 +5,32 @@
 #include <memory>
 #include <pthread.h>
 #include <string>
+#include <semaphore.h>
 
 namespace DW{
+    class Semaphore{
+    public:
+        Semaphore(const Semaphore&) = delete;
+        Semaphore(Semaphore&&) = delete;
+        Semaphore& operator==(const Semaphore&) = delete;
+
+        Semaphore()
+        Semaphore(uint32_t count);
+        ~Semaphore();
+
+        void wait();
+        void notify();
+
+    private:
+        sem_t m_semaphore;
+    }
+
     class Thread{
     public:
         using ptr = std::shared_ptr<Thread>;
 
         Thread(const Thread&) = delete;
-        Thread(const Thread&&) = delete;
+        Thread(Thread&&) = delete;
         Thread& operator==(const Thread&) = delete;
 
         Thread(std::function<void()> cb, const std::string& name);
@@ -21,6 +39,7 @@ namespace DW{
         pid_t getId() const { return m_id; };
         const std::string& getName() const { return m_name; };
         void join();
+        
         static Thread* GetThis();
         static const std::string& GetName();
         static void SetName(const std::string& name);
